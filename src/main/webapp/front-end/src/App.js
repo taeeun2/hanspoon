214f54532a773f45d3115007ff8c1cfad49d0118
Lee Hyun Ji <hyunjilee@hansol.com>
2022-06-27 15:18:24 +0900

    기능추가F: Material Kit 2 템플릿 적용

--- src/main/webapp/front-end/src/App.js
+++ src/main/webapp/front-end/src/App.js
@@ -1,52 +1,72 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Header from 'components/Header';
-import Home from 'pages/Home';
+import AboutUs from 'pages/LandingPages/AboutUs';
 import MyPage from 'pages/MyPage';
 import Post from 'pages/Post';
-import SignIn from 'pages/SignIn';
+import SignIn from 'pages/LandingPages/SignIn';
 import SignUp from 'pages/SignUp';
 import NotFound from 'pages/NotFound';
 
+// @mui material components
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+// Material Kit 2 React themes
+import theme from "assets/theme";
+import Presentation from "layouts/pages/presentation";
+
+// Material Kit 2 React routes
+import routes from "routes";
+
 
 const App = () => {
+	const { pathname } = useLocation();
+
+	// Setting page scroll to 0 when changing the route
+	useEffect(() => {
+	  document.documentElement.scrollTop = 0;
+	  document.scrollingElement.scrollTop = 0;
+	}, [pathname]);
+  
+	const getRoutes = (allRoutes) =>
+	  allRoutes.map((route) => {
+		if (route.collapse) {
+		  return getRoutes(route.collapse);
+		}
+  
+		if (route.route) {
+		  return <Route exact path={route.route} element={route.component} key={route.key} />;
+		}
+  
+		return null;
+	  });
 
   return (
-		// <BrowserRouter>
-		// 	<Routes>
-				
-				
-		// 		<Route element={<Header/>}>
-				
-		// 			<Route path="/" element={<Home />}></Route>
-		// 			<Route path="/mypage/*" element={<MyPage />}></Route>
-		// 			<Route path="/post/*" element={<Post />}></Route>
-		// 			<Route path="*" element={<NotFound />}></Route>
-				
-		// 		</Route>
-		// 		{/* <Route path="/signin/*" element={<LoginComponent />}></Route> */}
-		// 		<Route path="/signin/*" element={<SignIn />}></Route>
-		// 		<Route path="/signup/*" element={<SignUp />}></Route>
-				
-		// 	</Routes>
-		// </BrowserRouter> 
-			<BrowserRouter>
-				<Header />
-				<Routes>
-					<Route path="/" element={<Home/>}></Route>
-					<Route path="/mypage/*" element={<MyPage/>}></Route> 
-					<Route path="/post/*" element={<Post/>}></Route>
-					<Route path="/signin/*" element={<SignIn />}></Route>
-					<Route path="/signup/*" element={<SignUp />}></Route>
-					<Route path="*" element={<NotFound />}></Route>
-				</Routes>
-			</BrowserRouter>
-			
-			
-			
-
-
-	// 	</div>
+			// <BrowserRouter>
+			// 	<Header />
+			// 	<Routes>
+			// 		<Route path="/" element={<Home/>}></Route>
+			// 		<Route path="/mypage/*" element={<MyPage/>}></Route> 
+			// 		<Route path="/post/*" element={<Post/>}></Route>
+			// 		<Route path="/signin/*" element={<SignIn />}></Route>
+			// 		<Route path="/signup/*" element={<SignUp />}></Route>
+			// 		<Route path="*" element={<NotFound />}></Route>
+			// 	</Routes>
+			// </BrowserRouter>
+		
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<Routes>
+			  {/* {getRoutes(routes)} */}
+			  <Route path="/" element={<Presentation />} />
+			  <Route path="/mypage/*" element={<AboutUs/>} />
+			  <Route path="/post/*" element={<Post/>} />
+			  <Route path="/signin/*" element={<SignIn />} />
+			  <Route path="/signup/*" element={<SignUp />} />
+			  <Route path="*" element={<Navigate to="/" />} />
+			</Routes>
+		  </ThemeProvider>
   );
 }
 
--- src/main/webapp/front-end/src/pages/Home.js
+++ src/main/webapp/front-end/src/pages/Home.js
@@ -1,13 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import Blogs from 'containers/Blogs'
-import Banner from 'containers/Banner';
-import HomeLNB from 'containers/HomeLNB';
-import 'pages/css/Home.css'
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+// import Card from "@mui/material/Card";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+import MKTypography from "components/MKTypography";
+import MKButton from "components/MKButton";
+
+// Material Kit 2 React examples
+// import DefaultNavbar from "examples/Navbars/DefaultNavbar";
+// import DefaultFooter from "examples/Footers/DefaultFooter";
+
+// About Us page sections
+// import Information from "pages/LandingPages/AboutUs/sections/Information";
+// import Team from "pages/LandingPages/AboutUs/sections/Team";
+// import Featuring from "pages/LandingPages/AboutUs/sections/Featuring";
+// import Newsletter from "pages/LandingPages/AboutUs/sections/Newsletter";
+
+// Routes
+// import footerRoutes from "footer.routes";
+
+// Images
+import bgImage from "assets/images/bg-about-us.jpg";
 
 
 
 const Home = () => {
 
+    /* 로그인 관련 코드 */
     const [isLogin, setIsLogin] = useState(false)
 
 	useEffect(()=>{
@@ -21,15 +43,90 @@ const Home = () => {
 
 
     return (
-        <>
-           
+        
+    /* Material Kit 2 React(About Us) */
+      //   <>
+      // <MKBox
+      //   minHeight="75vh"
+      //   width="100%"
+      //   sx={{
+      //     backgroundImage: ({ functions: { linearGradient, rgba }, palette: { gradients } }) =>
+      //       `${linearGradient(
+      //         rgba(gradients.dark.main, 0.6),
+      //         rgba(gradients.dark.state, 0.6)
+      //       )}, url(${bgImage})`,
+      //     backgroundSize: "cover",
+      //     backgroundPosition: "center",
+      //     display: "grid",
+      //     placeItems: "center",
+      //   }}
+      // >
+      //   <Container>
+      //     <Grid
+      //       container
+      //       item
+      //       xs={12}
+      //       lg={8}
+      //       justifyContent="center"
+      //       alignItems="center"
+      //       flexDirection="column"
+      //       sx={{ mx: "auto", textAlign: "center" }}
+      //     >
+      //       <MKTypography
+      //         variant="h1"
+      //         color="white"
+      //         sx={({ breakpoints, typography: { size } }) => ({
+      //           [breakpoints.down("md")]: {
+      //             fontSize: size["3xl"],
+      //           },
+      //         })}
+      //       >
+      //         Work with an amazing design
+      //       </MKTypography>
+      //       <MKTypography variant="body1" color="white" opacity={0.8} mt={1} mb={3}>
+      //         We&apos;re constantly trying to express ourselves and actualize our dreams. If you
+      //         have the opportunity to play this game
+      //       </MKTypography>
+      //       <MKButton color="default" sx={{ color: ({ palette: { dark } }) => dark.main }}>
+      //         create account
+      //       </MKButton>
+      //       <MKTypography variant="h6" color="white" mt={8} mb={1}>
+      //         Find us on
+      //       </MKTypography>
+      //       <MKBox display="flex" justifyContent="center" alignItems="center">
+      //         <MKTypography component="a" variant="body1" color="white" href="#" mr={3}>
+      //           <i className="fab fa-facebook" />
+      //         </MKTypography>
+      //         <MKTypography component="a" variant="body1" color="white" href="#" mr={3}>
+      //           <i className="fab fa-instagram" />
+      //         </MKTypography>
+      //         <MKTypography component="a" variant="body1" color="white" href="#" mr={3}>
+      //           <i className="fab fa-twitter" />
+      //         </MKTypography>
+      //         <MKTypography component="a" variant="body1" color="white" href="#">
+      //           <i className="fab fa-google-plus" />
+      //         </MKTypography>
+      //       </MKBox>
+      //     </Grid>
+      //   </Container>
+      // </MKBox>
 
-            <Banner />
-            <HomeLNB />
-            <Blogs/>
-            
 
-        </>
+      {/* <Card
+        sx={{
+          p: 2,
+          mx: { xs: 2, lg: 3 },
+          mt: -8,
+          mb: 4,
+          boxShadow: ({ boxShadows: { xxl } }) => xxl,
+        }}
+      >
+        <Information />
+        <Team />
+        <Featuring />
+        <Newsletter />
+      </Card> */}
+        // </>
     )
 }
 
