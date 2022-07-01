54807fe9f72bab1152d2d73112ce586d974a8fe3
taeeun2 <taeeun.kim@hansol.com>
2022-07-01 18:17:59 +0900

    버그 : 도메인 생성자 버그 수정

--- src/main/java/com/hansol/hanspoon/entity/PostUser.java
+++ src/main/java/com/hansol/hanspoon/entity/PostUser.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 
 @Getter
 @Entity
-@Builder
+@NoArgsConstructor
 public class PostUser {
 
     @Id
@@ -29,6 +29,21 @@ public class PostUser {
 
     private long user_id;
     private long post_id;
+
+    @Builder
+    public PostUser(StatePostUserType state, boolean scope_gender
+            ,boolean scope_age,boolean scope_company, boolean scope_position, boolean scope_name
+    ,boolean scope_department, long user_id,long post_id){
+        this.state = state;
+        this.scope_age = scope_age;
+        this.scope_gender = scope_gender;
+        this.scope_company = scope_company;
+        this.scope_department = scope_department;
+        this.scope_name = scope_name;
+        this.user_id = user_id;
+        this.post_id = post_id;
+        this.scope_position = scope_position;
+    }
 
 
 }
