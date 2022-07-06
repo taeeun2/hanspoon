d6d02b7f4be3b10a46d7e518ac1d76b3d58f0efc
taeeun2 <taeeun.kim@hansol.com>
2022-07-06 15:09:24 +0900

    기능 추가F : 루트 추가

--- src/main/webapp/front-end/src/components/StaticBtn.js
+++ src/main/webapp/front-end/src/components/StaticBtn.js
@@ -1,15 +1,17 @@
 import { Box } from '@mui/system';
 import React from 'react';
 import Grid from "@mui/material/Grid";
+import { Link } from 'react-router-dom';
 
 function StaticBtn(props) {
     return (
         <Grid item xs={12} lg={12} className="static_button">
             <Box sx={{ width: '100%',
                      justifyContent: 'flex-end' }} className="static_button_box">
-                <button type="button" className='static_button_create'>
+                <Link to="/post"> <button type="button" className='static_button_create'>
                     모임 생성
-                </button>
+                </button></Link>
+               
                 <a href='#banner'>
                     <button type="button" className='static_button_up'>
                     </button>
